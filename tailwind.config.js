05e390c4d365c89d1df41572b25876d38aaf5bdf
Jot Hogben <jot_hogben@hotmail.co.uk>
2024-06-27 19:28:23 +0100

    tailwind styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["index.html", "./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        tabletText: "434px",
+      },
+      colors: {
+        bananaYellow: "#F4D04E",
+        powderBlack: "#111111",
+        platinum: "#7F7F7F",
+      },
+      fontFamily: {
+        body: ["Figtree"],
+      },
+      height: {
+        cardImg: "200px",
+      },
+      borderRadius: {
+        card: "1.25rem",
+        cardImg: "0.625rem",
+      },
+      boxShadow: {
+        card: "1rem 1rem 0 0 #000000",
+        cardMobile: "0.5rem 0.5rem 0 0 #000000",
+      },
+    },
   },
   plugins: [],
 };
